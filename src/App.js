86cfeee7f43acfd188b31652d0064c808d1be91e
cem <cemyilmaztech@gmail.com>
2024-09-13 18:49:58 +0300

    FlexAndResponsive component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import FlexAndResponsive from "./components/FlexAndResponsive";
 import HoverAndFocus from "./components/HoverAndFocus";
 import SpacingAndSizing from "./components/SpacingAndSizing";
 import Typography from "./components/Typography";
@@ -8,7 +9,8 @@ function App() {
       <h1 className="text-center text-4xl text-purple-700">Merhaba Tailwind</h1>
       {/* <Typography/> */}
       {/* <SpacingAndSizing/> */}
-      <HoverAndFocus/>
+      {/* <HoverAndFocus/> */}
+      <FlexAndResponsive/>
     </div>
   );
 }
--- src/components/FlexAndResponsive.jsx
+++ src/components/FlexAndResponsive.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const FlexAndResponsive = () => {
   return (
-    <div>FlexAndResponsive</div>
+    <div>
+        <div className='flex flex-wrap'>
+<div className='basis-1/4 bg-fuchsia-200'>01</div>
+<div className='basis-1/4 bg-fuchsia-300'>02</div>
+<div className='basis-1/4 bg-fuchsia-400'>03</div>
+<div className='basis-1/4 bg-fuchsia-500'>04</div>
+<div className='basis-2/4 bg-fuchsia-500'>05</div>
+<div className='basis-2/4 bg-fuchsia-500'>06</div>
+
+
+
+        </div>
+    </div>
   )
 }
 
