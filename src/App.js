fb69c62b345b8f656baaf476ab92998450c8d595
cem <cemyilmaztech@gmail.com>
2024-09-13 19:19:17 +0300

    dark theme

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import FlexAndResponsive from "./components/FlexAndResponsive";
 import HoverAndFocus from "./components/HoverAndFocus";
+import Navbar from "./components/Navbar";
 import SpacingAndSizing from "./components/SpacingAndSizing";
 import Typography from "./components/Typography";
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <h1 className="text-center text-4xl text-purple-700">Merhaba Tailwind</h1>
       {/* <Typography/> */}
       {/* <SpacingAndSizing/> */}
       {/* <HoverAndFocus/> */}
-      <FlexAndResponsive/>
+      {/* <FlexAndResponsive/> */}
     </div>
   );
 }
