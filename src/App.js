61ca463477bea70eb8159b9467d04ef10489aaa3
cem <cemyilmaztech@gmail.com>
2024-09-13 16:08:30 +0300

    line-clamp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Typography from "./components/Typography";
+
 function App() {
   return (
     <div className="App">
-      <h1>Cohort 17</h1>
+      <h1 className="text-center text-4xl text-purple-700">Merhaba Tailwind</h1>
+      <Typography/>
     </div>
   );
 }
---
+++ src/components/Typography.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Typography = () => {
+  return (
+    <div>
+        <p className='text-xs'>Typography</p>
+        <p className='text-sm'>Typography</p>
+        <p className='text-base'>Typography</p>
+        <p className='text-lg'>Typography</p>
+        <p className='text-2xl'>Typography</p>
+        <p className='text-4xl'>Typography</p>
+        <p className='text-6xl'>Typography</p>
+        <p className='text-8xl'>Typography</p>
+        <h3 className='text-[60px]'>Typography</h3>
+        <h4 className='text-8xl'>Typography</h4>
+        <p className='text-[20px]'>Typography</p>
+        <br />
+
+
+
+        <p className='line-clamp-3'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis accusamus perspiciatis consequuntur voluptate. Quidem nulla doloribus similique voluptatem quos, sed id fugit fuga eligendi, nam tempore culpa dolorum fugiat delectus, eveniet explicabo a quae officiis tempora excepturi? Dolor ipsum velit nulla quos illum! Nesciunt vero sequi qui earum excepturi unde.</p>
+    </div>
+  )
+}
+
+export default Typography
